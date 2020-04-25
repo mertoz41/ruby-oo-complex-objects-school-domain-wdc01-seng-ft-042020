3cd02c2e5c8aa8f03ca0718e8e9ee33736d70768
mertoz41 <mertozkaynak@windowslive.com>
2020-04-25 13:30:08 -0400

    first commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+    # attr_reader :roster
+    # attr_writer :grade
+    
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster 
+        @roster
+    end 
+
+    def add_student= (name, grade)
+        @roster[grade] = []
+        @roster[grade] << name 
+    end 
+
+
+
+
+end 
+
+School.add_student("mert ozkaynak", 12)
+School.roster
+
+
+
+
